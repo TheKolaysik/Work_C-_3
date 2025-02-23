42542968f3fef0d81ff717978efedc7c148d7b9c
TheKolaysik <tolstyhnikolai@gmail.com>
2025-02-23 17:01:11 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,164 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+float** create_matrix(int size);
+void print_matrix(float** matrix, int size);
+float** reverse_matrix(float** matrix, int size);
+int minor_matrix(float** matrix, int i, int j, int size);
+int determ_matrix(float** matrix, int size);
+void trans_matrix(float** matrix, int size);
+float** m_p_matrix(float** one_matrix, float** two_matrix, int size);
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	const int n = 4;
+	float** matrix = create_matrix(n);
+	float** trans_matrix = reverse_matrix(matrix, n);
+	float** multi_matrix = m_p_matrix(matrix, trans_matrix, n);
+	cout << "Исходная матрица" << endl;
+	print_matrix(matrix, n);
+	print_matrix(trans_matrix, n);
+
+}
+
+float** create_matrix(int size) {
+	float** matrix = new float* [size];
+	for (int i = 0; i < size; i++) {
+		matrix[i] = new float[size];
+	}
+	int x_coord = 0, y_coord = 0, count = 1;
+	for (x_coord; x_coord < size; x_coord++) {
+		matrix[y_coord][x_coord] = count;
+		count++;
+	}
+	x_coord--;
+	for (y_coord; y_coord < size; y_coord++) {
+		matrix[y_coord][x_coord] = count;
+		count++;
+	}
+	y_coord--;
+	x_coord--;
+	for (x_coord; x_coord >= 0; x_coord--) {
+		matrix[y_coord][x_coord] = count;
+		count++;
+	}
+	x_coord++;
+	y_coord--;
+	for (y_coord; y_coord > 0; y_coord--) {
+		matrix[y_coord][x_coord] = count;
+		count++;
+	}
+	y_coord++;
+	for (x_coord; x_coord < size - 1; x_coord++) {
+		matrix[y_coord][x_coord] = count;
+		count++;
+	}
+	x_coord--;
+	for (y_coord; y_coord < size - 1; y_coord++) {
+		matrix[y_coord][x_coord] = count;
+		count++;
+	}
+	matrix[y_coord - 1][x_coord - 1] = count;
+	return matrix;
+}
+
+void print_matrix(float** matrix, int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			cout << matrix[i][j] << '\t';
+		}
+		cout << endl;
+	}
+}
+
+float** reverse_matrix(float** matrix, int size) {
+	cout << "trans" << endl;
+	float** tran_matrix = new float* [size];
+	for (int i = 0; i < size; i++) {
+		tran_matrix[i] = new float[size];
+	}
+	int det_matrix = determ_matrix(matrix, size);
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			tran_matrix[i][j] = pow(-1, i + j + 2) * minor_matrix(matrix, i, j, size) / det_matrix;
+		}
+	}
+	trans_matrix(tran_matrix, size);
+	return tran_matrix;
+}
+
+int minor_matrix(float** matrix, int i, int j, int size) {
+	cout << "minor" << endl;
+	float** m_matrix = new float* [size - 1];
+	int determ, x_new = 0, y_new = 0;
+	for (int i = 0; i < size - 1; i++) {
+		m_matrix[i] = new float[size - 1];
+	}
+	for (int y = 0; y < size; y++) {
+		if (y == i) {
+			continue;
+		}
+		y_new = 0;
+		for (int x = 0; x < size; x++) {
+			if (x == j) {
+				continue;
+			}
+			cout << x << " " << y << " " << x_new << " " << y_new << endl;
+			m_matrix[y_new][x_new] = matrix[y][x];
+			cout << m_matrix[y_new][x_new] << endl;
+			y_new++;
+		}
+		x_new++;
+	}
+	cout << "minor_matrix" << endl;
+	determ = m_matrix[0][0] * m_matrix[1][1] * m_matrix[2][2] + m_matrix[0][1] * m_matrix[1][2] * m_matrix[2][0] +
+		+ m_matrix[0][2] * m_matrix[1][0] * m_matrix[2][1] - m_matrix[0][2] * m_matrix[1][1] * m_matrix[2][0] -
+		- m_matrix[0][0] * m_matrix[1][2] * m_matrix[2][1] - m_matrix[0][1] * m_matrix[1][0] * m_matrix[2][2];
+	for (int i = 0; i < size - 1; i++) {
+		cout << i;
+		delete[] m_matrix[i];
+	}
+	cout << "+";
+	delete[] m_matrix;
+	return determ;
+}
+
+int determ_matrix(float** matrix, int size) {
+	int determ = 0;
+	int i = 0;
+	for (int j = 0; j < size; j++) {
+		determ += matrix[i][j] * minor_matrix(matrix, i, j, size) * pow(-1, j + 2);
+	}
+	return determ;
+}
+
+void trans_matrix(float** matrix, int size) {
+	cout << "trans" << endl;
+	int count = 1;
+	float temp;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < count; j++) {
+			temp = matrix[i][j];
+			matrix[i][j] = matrix[j][i];
+			matrix[j][i] = temp;
+		}
+		count++;
+	}
+}
+
+float** m_p_matrix(float** one_matrix, float** two_matrix, int size) {
+	int x = 0, y = 0;
+	float res;
+	float** multiplication = new float* [size];
+	for (int i = 0; i < size; i++) {
+		multiplication[i] = new float[size];
+	}
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			for (int k = 0; k < size; k++) {
+				one_matrix[i][j]
+			}
+		}
+	}
+}
