0409792527e94e2e9109aeeae6e8bb5484298ac3
TheKolaysik <tolstyhnikolai@gmail.com>
2025-02-27 20:59:07 +0300

    Ошибки исправлены, добавлены коментарии функциям

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include <cmath>
 using namespace std;
 
-float** create_matrix(int size);
-void print_matrix(float** matrix, int size);
-float** reverse_matrix(float** matrix, int size);
-int minor_matrix(float** matrix, int i, int j, int size);
-int determ_matrix(float** matrix, int size);
-void trans_matrix(float** matrix, int size);
-float** m_p_matrix(float** one_matrix, float** two_matrix, int size);
+float** create_matrix(int size); // Инициализация исходной матрицы
+void print_matrix(float** matrix, int size, bool exp_m); // Вывод матрицы в консоль
+float** reverse_matrix(float** matrix, int size); // Обратная матрица
+int minor_matrix(float** matrix, int i, int j, int size); // Минор матрицы
+int determ_matrix(float** matrix, int size); // Определитель матрицы
+void trans_matrix(float** matrix, int size); // Транспонирование матрицы
+float** m_p_matrix(float** one_matrix, float** two_matrix, int size); // Умножение одной матрицы на другую
 
 int main() {
 	setlocale(LC_ALL, "RUS");
@@ -17,11 +17,11 @@ int main() {
 	float** trans_matrix = reverse_matrix(matrix, n);
 	float** multi_matrix = m_p_matrix(matrix, trans_matrix, n);
 	cout << "Исходная матрица" << endl;
-	print_matrix(matrix, n);
+	print_matrix(matrix, n, false);
 	cout << "Обратная матрица" << endl;
-	print_matrix(trans_matrix, n);
+	print_matrix(trans_matrix, n, true);
 	cout << "Результат умножения" << endl;
-	print_matrix(multi_matrix, n);
+	print_matrix(multi_matrix, n, false);
 	for (int i = 0; i < n; i++) {
 		delete[] matrix[i];
 		delete[] trans_matrix[i];
@@ -32,6 +32,7 @@ int main() {
 	delete[] multi_matrix;
 }
 
+// Инициализация матрицы
 float** create_matrix(int size) {
 	float** matrix = new float* [size];
 	for (int i = 0; i < size; i++) {
@@ -43,7 +44,7 @@ float** create_matrix(int size) {
 		count++;
 	}
 	x_coord--;
-	for (y_coord; y_coord < size; y_coord++) {
+	for (y_coord++; y_coord < size; y_coord++) {
 		matrix[y_coord][x_coord] = count;
 		count++;
 	}
@@ -60,12 +61,12 @@ float** create_matrix(int size) {
 		count++;
 	}
 	y_coord++;
-	for (x_coord; x_coord < size - 1; x_coord++) {
+	for (x_coord++; x_coord < size - 1; x_coord++) {
 		matrix[y_coord][x_coord] = count;
 		count++;
 	}
 	x_coord--;
-	for (y_coord; y_coord < size - 1; y_coord++) {
+	for (y_coord++; y_coord < size - 1; y_coord++) {
 		matrix[y_coord][x_coord] = count;
 		count++;
 	}
@@ -73,17 +74,28 @@ float** create_matrix(int size) {
 	return matrix;
 }
 
-void print_matrix(float** matrix, int size) {
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			cout << matrix[i][j] << '\t';
+// Вывод матрицы в консоль
+void print_matrix(float** matrix, int size, bool exp_m) {
+	if (!exp_m) {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				printf("%.0f\t", matrix[i][j]);
+			}
+			cout << endl;
 		}
-		cout << endl;
+	}
+	else {
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				cout << matrix[i][j] << "\t";
+			}
+			cout << endl;
+		}
 	}
 }
 
+// Инициализация обратной матрицы
 float** reverse_matrix(float** matrix, int size) {
-	cout << "trans" << endl;
 	float** tran_matrix = new float* [size];
 	for (int i = 0; i < size; i++) {
 		tran_matrix[i] = new float[size];
@@ -91,13 +103,14 @@ float** reverse_matrix(float** matrix, int size) {
 	int det_matrix = determ_matrix(matrix, size);
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
-			tran_matrix[i][j] = pow(-1, i + j + 2) * minor_matrix(matrix, i, j, size) / det_matrix;
+			tran_matrix[i][j] = pow(-1, i + j) * minor_matrix(matrix, i, j, size) / det_matrix;
 		}
 	}
 	trans_matrix(tran_matrix, size);
 	return tran_matrix;
 }
 
+// Минор матрицы
 int minor_matrix(float** matrix, int i, int j, int size) {
 	float** m_matrix = new float* [size - 1];
 	int determ, x_new = 0, y_new = 0;
@@ -108,20 +121,19 @@ int minor_matrix(float** matrix, int i, int j, int size) {
 		if (y == i) {
 			continue;
 		}
-		y_new = 0;
+		x_new = 0;
 		for (int x = 0; x < size; x++) {
 			if (x == j) {
 				continue;
 			}
-			//cout << x << " " << y << " " << x_new << " " << y_new << endl;
 			m_matrix[y_new][x_new] = matrix[y][x];
-			y_new++;
+			x_new++;
 		}
-		x_new++;
+		y_new++;
 	}
-	determ = m_matrix[0][0] * m_matrix[1][1] * m_matrix[2][2] + m_matrix[0][1] * m_matrix[1][2] * m_matrix[2][0] +
-		+ m_matrix[0][2] * m_matrix[1][0] * m_matrix[2][1] - m_matrix[0][2] * m_matrix[1][1] * m_matrix[2][0] -
-		- m_matrix[0][0] * m_matrix[1][2] * m_matrix[2][1] - m_matrix[0][1] * m_matrix[1][0] * m_matrix[2][2];
+	determ = m_matrix[0][0] * m_matrix[1][1] * m_matrix[2][2] + m_matrix[1][0] * m_matrix[2][1] * m_matrix[0][2];
+	determ += m_matrix[2][0] * m_matrix[0][1] * m_matrix[1][2] - m_matrix[2][0] * m_matrix[1][1] * m_matrix[0][2];
+	determ +=  -m_matrix[0][0] * m_matrix[1][2] * m_matrix[2][1] - m_matrix[1][0] * m_matrix[0][1] * m_matrix[2][2];
 	for (int i = 0; i < size - 1; i++) {
 		delete[] m_matrix[i];
 	}
@@ -129,15 +141,17 @@ int minor_matrix(float** matrix, int i, int j, int size) {
 	return determ;
 }
 
+// Определитель матрицы
 int determ_matrix(float** matrix, int size) {
 	int determ = 0;
 	int i = 0;
 	for (int j = 0; j < size; j++) {
-		determ += matrix[i][j] * minor_matrix(matrix, i, j, size) * pow(-1, j + 2);
+		determ += matrix[i][j] * minor_matrix(matrix, i, j, size) * pow(-1, j);
 	}
 	return determ;
 }
 
+// Транспонирование матрицы
 void trans_matrix(float** matrix, int size) {
 	int count = 1;
 	float temp;
@@ -151,6 +165,7 @@ void trans_matrix(float** matrix, int size) {
 	}
 }
 
+// Умножение одной матрицы на другую
 float** m_p_matrix(float** one_matrix, float** two_matrix, int size) {
 	float res;
 	float** multiplication = new float* [size];
